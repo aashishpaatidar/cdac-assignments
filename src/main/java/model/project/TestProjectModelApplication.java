887cfb69288e8e060b80cc9269e2af980743f7cb
Ashish Patidar <ashishpatidar365@gmail.com>
2021-01-10 14:14:32 +0530

    ashish

--- src/main/java/model/project/TestProjectModelApplication.java
+++ src/main/java/model/project/TestProjectModelApplication.java
@@ -3,7 +3,6 @@ package model.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//main method WOW
 @SpringBootApplication
 public class TestProjectModelApplication {
 
